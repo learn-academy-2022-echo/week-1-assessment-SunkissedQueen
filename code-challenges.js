28344aa167e3f1e1489e8b7ba0cf9c959aa3ee07
Charlean Baxter <baxtercm@outlook.com>
2022-08-09 10:43:56 -0700

    completed week 1 assessment with the aid of 2022E cohort

--- code-challenges.js
+++ code-challenges.js
@@ -31,41 +31,81 @@ const stringOne = (word1, word2) => {
 }
 console.log(stringOne(fruit1, fruit2))
 console.log(stringOne(fruit3, fruit4))
-// --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
+// --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+// From Web Developer Brook
 // Pseudo code:
 
-const temp1 = 42
-const temp2 = 350
-const temp3 = 212
+  //Function: boiling point
+  //parameter: Any given number
+ //expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+ //conditional statements:if the temperature is at boiling point it will return its at its boiling point. If its above it boiling point it will return its above its boiling point. If it below its temp it will return below temp
+
+
+ const temp1 = 42
+ const temp2 = 350
+ const temp3 =  212
+
+const boilingPoint = (temps) => {
+   if(temps < 212) {
+   return "Its below boilingpoint"
+  } else if(temps > 212) {
+    return "Its above boilingpoint"
+   }else if(temps === 212) {
+    return "Its hot in here"
+   }
+ }
+    console.log(boilingPoint(temp1))
+   console.log(boilingPoint(temp2))
+    console.log(boilingPoint(temp3))
 
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
-
-// Pseudo code:
+// From Developer Daniel
+// Pseudo code: 
+// Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+let bothNumArr = myNumbers1.concat(myNumbers2)
+console.log(bothNumArr.length)
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 ohcE"
+// From Web Developer Namier
 
-// Pseudo code:
+// // Pseudo code:
+// Utilize .split and split each character into an array
+// Utilize .reverse to reverse the array
+// Utilize .join to create string 
 
 const currentCohort = "Echo 2022"
 
+console.log(currentCohort.split('').reverse().join(''))
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
-// Pseudo code:
+// From web Developer Tuan
+
+// Pseudo code: Using loops, the count of the loop will start at index 0 until 6, and after each loop it will output the string "odd" or "even" depending on the remainder of the modulo. If the remainder is 0 then its "even" and if the remained is not 0 then it will output "odd"
 
 const myArray = [13, 34, 5, 10, 27, 42]
+ for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] % 2 === 0) {
+    console.log('even')
+ } else {
+    console.log('odd')
+    } 
+}
 
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
+// From Developer Luis
 
 // Pseudo code:
-
+// create function w/ new var 
+// compare bigger num w/ small num > < 
+// return with it subtracting big - small
 // Set one:
 const number1 = 58
 const number2 = 100
@@ -73,3 +113,15 @@ const number2 = 100
 // Set two:
 const number3 = 27
 const number4 = 24
+
+const subTotal = (numInput1,numInput2) => {
+  if (numInput1 < numInput2) { 
+    return (numInput2 - numInput1)
+  } else if (numInput1 > numInput2) {
+   return numInput1 - numInput2
+  }
+}
+
+console.log(subTotal(number1,number2)) ;
+console.log(subTotal(number3,number4))
+
--- coding-concepts.js
+++ coding-concepts.js
@@ -16,19 +16,19 @@ console.log(colors.push("indigo"))
 // --------------------1) What will this log?
 
 const cohort = "LEARN 2022"
-// console.log(cohort.length)
-
-// a) Your answer:
-// b) Verify and explain:
+console.log(cohort.length)
+// From Developer Jereme
+// a) Your answer: 10
+// b) Verify and explain: counts all the characters even the spaces
 
 
 // --------------------2) What will this log?
 
 const greeting = "Hello World!"
 // console.log(greeting[4])
-
-// a) Your answer:
-// b) Verify and explain:
+// From Developer Jason
+// a) Your answer: "o"
+// b) Verify and explain: The 4th index has a value of "o"
 
 
 // --------------------3) What will this log?
@@ -37,8 +37,9 @@ const languages = ["JavaScript", "Ruby", "Python", "C++"]
 const index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// From Web Developer Vanessa, confirmed by Developer Joseph
+// a) Your answer: This will return "Ruby" because it is asking for the entry in index 1
+// b) Verify and explain: This is correct, it returns Ruby
 
 
 // --------------------4) What will this log?
@@ -46,14 +47,15 @@ const index = 1
 const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// From Developer Joyce
+// a) Your answer: initially thought the values would be in caps
+// b) Verify and explain: received an error message; cannot use .toUpperCase() on the array. Need to switch to a string datatype
 
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
-
-// a) Your answer:
-// b) Verify and explain:
+// From Developer Dawit
+// a) Your answer: 6, thought it would count the last string in the array
+// b) Verify and explain: number, dataTypes.length will give 3 for the amount of values in the array. type of will state that the datatype of 3 is a number.
